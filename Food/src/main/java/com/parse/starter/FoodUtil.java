5d4bb49323330c2126b2aa8b4ad84cbaeb5c8c6f
LeiPei <hileopei@gmail.com>
2016-11-25 12:42:25 -0800

    added maps to FoodUtil

--- Food/src/main/java/com/parse/starter/FoodUtil.java
+++ Food/src/main/java/com/parse/starter/FoodUtil.java
@@ -4,7 +4,9 @@ import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by PeiLei on 11/19/16.
@@ -12,6 +14,8 @@ import java.util.List;
 
 public class FoodUtil {
 	private static ArrayList<String> allFood;
+	private static Map<String, String> allUnit;
+	private static Map<String, String> allCalorie;
 
 	/**
 	 * get all the food names.
@@ -28,5 +32,42 @@ public class FoodUtil {
 			e.printStackTrace();
 		}
 		return allFood;
+	}
+
+
+	/**
+	 * get all the food units
+	 * @return the map containing all the food units
+	 */
+	public static Map<String, String> getAllUnit() {
+		allUnit = new HashMap<String, String>();
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("Food");
+		try {
+			List<ParseObject> objects = query.find();
+			for(ParseObject object : objects) {
+				allUnit.put(object.get("name").toString(), object.get("unit").toString());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return allUnit;
+	}
+
+	/**
+	 * get all the food Calories
+	 * @return the map containing all the food units
+	 */
+	public static Map<String, String> getAllCalorie() {
+		allCalorie = new HashMap<String, String>();
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("Food");
+		try {
+			List<ParseObject> objects = query.find();
+			for(ParseObject object : objects) {
+				allUnit.put(object.get("name").toString(), object.get("calorie").toString());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return allCalorie;
 	}
 }
